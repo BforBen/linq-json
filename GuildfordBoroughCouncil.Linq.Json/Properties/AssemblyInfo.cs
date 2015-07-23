55891228ccddd4bdc8387e68dcf19279ba67b168
Ben <BforBen@users.noreply.github.com>
2015-07-23 15:00:41 +0100

    Initial commit

---
+++ GuildfordBoroughCouncil.Linq.Json/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GuildfordBoroughCouncil.Linq.Json")]
+[assembly: AssemblyDescription("A collection of Linq extension methods for JSON.")]
+[assembly: AssemblyCompany("Guildford Borough Council")]
+[assembly: AssemblyProduct("GuildfordBoroughCouncil.Linq.Json")]
+[assembly: AssemblyCopyright("Copyright © Ben Cheetham 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28a5fc35-3613-44be-ae1b-8b1e36467e50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
